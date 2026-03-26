5ecab008d8886c76e54caaef909e69c50459a36d
Charchit-Chaudharygit config --global user.email charchit12356@gmail.com <charchit12356@gmail.com>
2026-03-26 13:47:59 +0545

    Data Types

---
+++ Data Types.js
@@ -0,0 +1,19 @@
+// Data Types are values that we can assign to variables. They determine the type of data that can be stored in a variable and how it can be used. In JavaScript, there are several data types, including:
+// 1. String: A sequence of characters enclosed in single or double quotes. Example: "Hello, World!"
+let greeting = "Hello, World!";
+// 2. Number: Represents both integer and floating-point numbers. Example: 42, 3.14
+let age = 30;
+// 3. Boolean: Represents a logical value that can be either true or false. Example: true, false
+let isStudent = true;
+// 4. Null: Represents the intentional absence of any object value. Example: null
+let emptyValue = null;
+// 5. Undefined: Represents a variable that has been declared but has not been assigned a value. Example: undefined
+let uninitializedVariable;
+// 6. Object: Represents a collection of properties and values. Example: { name: "Alice", age: 25 }
+let person = { name: "Alice", age: 25 };
+// 7. Symbol: Represents a unique identifier. Example: Symbol("id")
+// let uniqueId = Symbol("id");
+// Template literals are a way to create strings in JavaScript that allow for embedded expressions. They are enclosed in backticks (`) and can contain placeholders for variables or expressions, which are denoted by ${}. This allows for easier string concatenation and formatting. For example:
+let name ="charchit";
+let age1= 23;
+console.log(`My name is ${name} and I am ${age} years old.`);
